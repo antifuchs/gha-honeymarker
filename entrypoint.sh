17e792f416455b45c1b0aadc1875eda57088e30d
Andreas Fuchs <asf@materialize.com>
2021-06-04 15:28:29 -0400

    Add a URL argument to the marker

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,17 @@ function install_binary() {
     go get github.com/honeycombio/honeymarker
 }
 
+function marker_url() {
+    if [ -z "$INPUT_URL" ] ; then
+        curl -s "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}" | jq -r '.html_url'
+    else
+        echo "$INPUT_URL"
+    fi
+}
+
 function create_marker() {
-    honeymarker -k $INPUT_APIKEY -d $INPUT_DATASET add -t $INPUT_TYPE -m $INPUT_MESSAGE
+    honeymarker -k "$INPUT_APIKEY" -d "$INPUT_DATASET" add -t "$INPUT_TYPE" -m "$INPUT_MESSAGE" -u "$(marker_url)"
 }
 
 install_binary
-create_marker
+create_markers
